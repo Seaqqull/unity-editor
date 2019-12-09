270a3a851ac31284656c750b7895b22c96f88a3a
Dima <seaqqullprogramming@gmail.com>
2019-12-09 12:55:03 +0100

    Update: Read-only restriction
     Added:
     - Variable read-only attribute
     - Group read-only attribute

--- UnityEditor/Assets/Scripts/Properties/Variables/VariableContainer.cs
+++ UnityEditor/Assets/Scripts/Properties/Variables/VariableContainer.cs
@@ -2,6 +2,12 @@
 
 public class VariableContainer : MonoBehaviour
 {
+    [BeginReadOnlyGroup]
+    public string entityName = "Some entity name";
+    [ReadOnly] public int val = 7;
+    [EndReadOnlyGroup]
+
+
     public FloatReference Float;
     public IntegerReference Int;
     [TagSelector] public string[] Tags;
